b2e369cfc41bd985c31ac76b4f28e87c61262d66
Keshav Reddy <keshavlingala@gmail.com>
2021-09-17 11:14:58 +0530

    Project Inited

---
+++ capstone-project-dbs/src/main/java/com/teamerror/capstoneprojectdbs/CapstoneProjectDbsApplication.java
@@ -0,0 +1,13 @@
+package com.teamerror.capstoneprojectdbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneProjectDbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneProjectDbsApplication.class, args);
+	}
+
+}
---
+++ capstone-project-dbs/src/test/java/com/teamerror/capstoneprojectdbs/CapstoneProjectDbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamerror.capstoneprojectdbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneProjectDbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
