7b80a2ec48cb9642d7c3aaa7f49a02ec3f6a7f08
BommalaBharath <bharath.bommala@gmail.com>
2021-09-17 07:22:30 +0000

    moved resource folder

--- capstone-project-dbs/src/main/java/com/teamerror/services/ClientIdservice.java
+++ capstone-project-dbs/src/main/java/com/teamerror/services/ClientIdservice.java
@@ -5,22 +5,15 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.teamerror.capstoneprojectdbs.entities.Client;
+
 @Service
 public class ClientIdservice {
 	
-	@Autowired
-	ClientRepository clientRepository;
+	
 	
 	public Client findByClientid(String clientId)
 	{
-		Optional<Client> clientobj = clientRepository.findById(clientId);
-		
-		if(clientobj.isPresent()) {
-			return clientobj.get();
-		}
-		
-		else {
-			return null;
-		}
+		return null;
 	}
 }
