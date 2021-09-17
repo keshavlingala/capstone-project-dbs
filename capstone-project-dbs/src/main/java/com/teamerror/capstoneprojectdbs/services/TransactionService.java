5987fb3933593d90e698559c8b1c51493e018333
Keshav Reddy <keshavlingala@gmail.com>
2021-09-17 21:26:04 +0530

    Transaction Service Half Logic Done

--- capstone-project-dbs/src/main/java/com/teamerror/capstoneprojectdbs/services/TransactionService.java
+++ capstone-project-dbs/src/main/java/com/teamerror/capstoneprojectdbs/services/TransactionService.java
@@ -1,13 +1,24 @@
 package com.teamerror.capstoneprojectdbs.services;
 
+import com.teamerror.capstoneprojectdbs.entities.Client;
+import com.teamerror.capstoneprojectdbs.entities.Instrument;
+import com.teamerror.capstoneprojectdbs.entities.OrderBook;
+import com.teamerror.capstoneprojectdbs.entities.Stocks;
 import com.teamerror.capstoneprojectdbs.models.CustomResponse;
 import com.teamerror.capstoneprojectdbs.models.OrderBookRequest;
 import com.teamerror.capstoneprojectdbs.models.OrderDirection;
+import com.teamerror.capstoneprojectdbs.models.OrderStatus;
 import com.teamerror.capstoneprojectdbs.repositories.*;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -35,7 +46,69 @@ public class TransactionService {
 
 
     private ResponseEntity<Object> sellOrder(OrderBookRequest orderBookRequest) {
+        if (!instrumentRepository.findById(orderBookRequest.getInstrumentId()).isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new CustomResponse("Instrument Not Found", "Instrument Not Found"));
+        }
+        if (!clientRepository.findById(orderBookRequest.getClientId()).isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new CustomResponse("Client Not Found", "Client Not Found"));
+        }
+
+        Instrument instrument = instrumentRepository.findById(orderBookRequest.getInstrumentId()).get();
+        Client client = clientRepository.findById(orderBookRequest.getClientId()).get();
+
+        List<OrderBook> buyOrdersWithSameInstrument = orderBookRepository.findAllByOrderDirection(OrderDirection.BUY).stream().filter(record -> {
+            return record.getInstrumentId().getInstrumentId().equals(instrument.getInstrumentId());
+        }).collect(Collectors.toList());
+        if (buyOrdersWithSameInstrument.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.OK).body(saveOrderInOrderBook(orderBookRequest, instrument, client));
+        }
+        List<OrderBook> perfectBuyers = buyOrdersWithSameInstrument
+                .stream()
+                .filter(item -> item.getPrice().equals(orderBookRequest.getPrice())).collect(Collectors.toList());
+        if (!perfectBuyers.isEmpty()) {
+            // One Buyer Case
+            OrderBook minBuyer = perfectBuyers.stream().min(Comparator.comparing(OrderBook::getTimeStamp)).get();
+            if (minBuyer.getQuantity().equals(orderBookRequest.getQuantity())) {
+                // Perfect Buy Sell Trade
+                Client buyer = minBuyer.getClientId();
+                Stocks buyerStock = new Stocks();
+                buyerStock.setStockId(UUID.randomUUID());
+                buyerStock.setClientId(buyer);
+                buyerStock.setInstrumentId(minBuyer.getInstrumentId());
+                buyerStock.setQuantity(minBuyer.getQuantity());
+                // Buyer and Seller Transaction limit update
+                buyer.setTransactionLimit(buyer.getTransactionLimit() - minBuyer.getQuantity() * minBuyer.getPrice());
+                client.setTransactionLimit(client.getTransactionLimit() - minBuyer.getQuantity() * minBuyer.getPrice());
+
+                // TODO Unfinished
+
+                orderBookRepository.delete(minBuyer);
+                stocksRepository.save(buyerStock);
+
+
+            } else {
+                // TODO Match multiple Buyers to single Seller
+                List<OrderBook> buyers = perfectBuyers.stream()
+                        .filter(order -> order.getQuantity() < orderBookRequest.getQuantity()).collect(Collectors.toList());
+
+            }
+        }
+
+        buyOrdersWithSameInstrument.sort(Comparator.comparing(OrderBook::getTimeStamp));
         return null;
+    }
+
+    private OrderBook saveOrderInOrderBook(OrderBookRequest orderBookRequest, Instrument instrument, Client client) {
+        OrderBook orderBook = new OrderBook();
+        orderBook.setOrderId(UUID.randomUUID());
+        orderBook.setOrderStatus(OrderStatus.PROCESSING);
+        orderBook.setOrderDirection(OrderDirection.SELL);
+        orderBook.setQuantity(orderBookRequest.getQuantity());
+        orderBook.setLimitOrder(orderBookRequest.getLimitOrder());
+        orderBook.setInstrumentId(instrument);
+        orderBook.setClientId(client);
+        orderBook.setTimeStamp(new Date());
+        return orderBookRepository.save(orderBook);
     }
 
 }
