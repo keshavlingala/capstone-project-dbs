5fb6ab1c488b66e810b6261a1d3eb2ac95795f91
BommalaBharath <bharath.bommala@gmail.com>
2021-09-17 09:38:25 +0000

    updated Instrument Service

--- capstone-project-dbs/src/main/java/com/teamerror/capstoneprojectdbs/services/InstrumentService.java
+++ capstone-project-dbs/src/main/java/com/teamerror/capstoneprojectdbs/services/InstrumentService.java
@@ -1,7 +1,28 @@
 package com.teamerror.capstoneprojectdbs.services;
 
+import com.teamerror.capstoneprojectdbs.entities.Instrument;
+import com.teamerror.capstoneprojectdbs.repositories.InstrumentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class InstrumentService {
+
+    @Autowired
+    InstrumentRepository instrumentRepository;
+
+    public Instrument findbyInstrumentId(String instrumentId)
+    {
+        Optional<Instrument> instrumentobj = instrumentRepository.findById(instrumentId);
+
+        if(instrumentobj.isPresent()) {
+            return instrumentobj.get();
+        }
+
+        else {
+            return null;
+        }
+    }
 }
