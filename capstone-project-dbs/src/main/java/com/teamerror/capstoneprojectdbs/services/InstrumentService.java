5713e07b3f58e19c06060db304c061d00dfc7243
Keshav Reddy <keshavlingala@gmail.com>
2021-09-17 23:25:30 +0530

    StockService.java methods added

--- capstone-project-dbs/src/main/java/com/teamerror/capstoneprojectdbs/services/InstrumentService.java
+++ capstone-project-dbs/src/main/java/com/teamerror/capstoneprojectdbs/services/InstrumentService.java
@@ -2,13 +2,14 @@ package com.teamerror.capstoneprojectdbs.services;
 
 import com.teamerror.capstoneprojectdbs.entities.Instrument;
 import com.teamerror.capstoneprojectdbs.repositories.InstrumentRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class InstrumentService {
 
-    @Autowired
     InstrumentRepository instrumentRepository;
 
     public Instrument findByInstrumentId(String instrumentId) {
--- capstone-project-dbs/src/main/java/com/teamerror/capstoneprojectdbs/services/StockService.java
+++ capstone-project-dbs/src/main/java/com/teamerror/capstoneprojectdbs/services/StockService.java
@@ -4,19 +4,57 @@ import com.teamerror.capstoneprojectdbs.entities.Client;
 import com.teamerror.capstoneprojectdbs.entities.Instrument;
 import com.teamerror.capstoneprojectdbs.entities.Stocks;
 import com.teamerror.capstoneprojectdbs.repositories.StocksRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
+@AllArgsConstructor
 public class StockService {
 
-    @Autowired
     StocksRepository stocksRepository;
 
-    public Stocks saveStock(Client client, Instrument instrument,int quantity){
-        //Stocks stock = stocksRepository.findByClientIdAndInstrument(client,instrument);
+    /**
+     * save stock
+     *
+     * @param client     client
+     * @param instrument instrument
+     * @param quantity   quantity (Can be Positive or Negative)
+     * @return {@link Stocks} saves a particular stocks for user, if already exists adds quantity ( Quantity can be Negative i.e. reduce Stocks for that client )
+     * @see Stocks
+     */
+    public Stocks saveStock(Client client, Instrument instrument, Integer quantity) {
+        Stocks getOrCreate = stocksRepository.findByClientIdAndInstrument(client, instrument).orElseGet(() -> {
+            Stocks newStock = new Stocks();
+            newStock.setStockId(UUID.randomUUID());
+            newStock.setClientId(client);
+            newStock.setInstrumentId(instrument);
+            newStock.setQuantity(0);
+            return newStock;
+        });
+        getOrCreate.setQuantity(getOrCreate.getQuantity() + quantity);
+        return getOrCreate;
+    }
 
-        return null;
+
+    /**
+     * get stock quantity
+     *
+     * @param client     client
+     * @param instrument instrument
+     * @return {@link Stocks} Returns a Stock Object if found else Create an empty stock with 0 quantity and returns
+     * @see Stocks
+     */
+    Stocks getStockQuantity(Client client, Instrument instrument) {
+        return stocksRepository.findByClientIdAndInstrument(client, instrument).orElseGet(() -> {
+            Stocks newStock = new Stocks();
+            newStock.setStockId(UUID.randomUUID());
+            newStock.setClientId(client);
+            newStock.setInstrumentId(instrument);
+            newStock.setQuantity(0);
+            return newStock;
+        });
     }
 
 }
