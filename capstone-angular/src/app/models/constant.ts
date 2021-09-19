686cd9135713d88bb7d1a1617c67828b9d98ab5e
Saimahesh28 <54111877+saimahesh28@users.noreply.github.com>
2021-09-19 20:31:41 +0000

    added new urls in constant.ts

--- capstone-angular/src/app/models/constant.ts
+++ capstone-angular/src/app/models/constant.ts
@@ -4,3 +4,5 @@ export const API_URL = environment.BASE_URL
 export const GET_CLIENT_DATA = API_URL + 'client/'
 export const GET_INSTRUMENT_DATA = API_URL + 'instrument/'
 export const POST_TRANSACTION_DATA = API_URL + 'transaction/'
+export const CUSTODIAN_WISE_DATA = API_URL + 'custodianWiseStats/'
+export const CLIENT_WISE_DATA = API_URL + 'clientWiseStats/'
