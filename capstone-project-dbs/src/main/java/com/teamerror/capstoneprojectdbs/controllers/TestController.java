3a20be3ddc41affe149b594296c918c5b6d25e25
DosapatiMeghana <meghanadosapati369@gmail.com>
2021-09-19 04:25:50 +0000

    Made Changes in Test Controller

--- capstone-project-dbs/src/main/java/com/teamerror/capstoneprojectdbs/controllers/TestController.java
+++ capstone-project-dbs/src/main/java/com/teamerror/capstoneprojectdbs/controllers/TestController.java
@@ -1,6 +1,7 @@
 package com.teamerror.capstoneprojectdbs.controllers;
 
 import com.teamerror.capstoneprojectdbs.entities.Client;
+import com.teamerror.capstoneprojectdbs.entities.Instrument;
 import com.teamerror.capstoneprojectdbs.entities.Stocks;
 import com.teamerror.capstoneprojectdbs.repositories.StocksRepository;
 import org.springframework.http.HttpStatus;
@@ -10,14 +11,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class TestController {
         StocksRepository stocksRepository;
 
         @GetMapping(value="stocks/{id}")
-        public ResponseEntity<List<Stocks>> getCustomer(@PathVariable("id") Client id) {
+        public ResponseEntity<List<Stocks>> getStocksByClientId(@PathVariable("id") Client id) {
                 List<Stocks> stocks = stocksRepository.findAllByClient(id);
                 return new ResponseEntity<List<Stocks>>(stocks, HttpStatus.OK);
+        }
+
+        @GetMapping(value="stocks/{clientId,instrumentId}")
+        public ResponseEntity<Optional<Stocks>> getStocksByClientAndInstrument(@PathVariable("clientId") Client clientId,
+                                                        @PathVariable("instrumentId") Instrument instrumentId) {
+                Optional<Stocks> stocks = stocksRepository.findByClientAndInstrument(clientId, instrumentId);
+                return new ResponseEntity<Optional<Stocks>>(stocks, HttpStatus.OK);
         }
 }
