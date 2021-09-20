f1dfa56c28430f1c1dd60973728b348811826bb7
anirudh <anirudhreddybasani@gmail.com>
2021-09-20 11:40:51 +0530

    modified the dashboard queries to return data of only completed transactions

--- capstone-project-dbs/src/main/java/com/teamerror/capstoneprojectdbs/repositories/OrderBookRepository.java
+++ capstone-project-dbs/src/main/java/com/teamerror/capstoneprojectdbs/repositories/OrderBookRepository.java
@@ -16,15 +16,15 @@ public interface OrderBookRepository extends JpaRepository<OrderBook, Integer> {
     List<OrderBook> findAllByOrderDirection(OrderDirection orderDirection);
 
     @Query(value="select custodian_id as custodianId," +
-            "sum(IF(ob.order_direction='BUY',ob.price * initial_quantity,0)) as totalBuy," +
-            "sum(IF(ob.order_direction='SELL',ob.price * initial_quantity,0)) as totalSell " +
+            "sum(IF(ob.order_direction='BUY' and ob.order_status='COMPLETED',ob.price * initial_quantity,0)) as totalBuy," +
+            "sum(IF(ob.order_direction='SELL' and ob.order_status='COMPLETED',ob.price * initial_quantity,0)) as totalSell " +
             "from client c left join order_book ob on c.client_id = ob.client_id " +
             "group by custodian_id",nativeQuery = true)
     List<CustodianWiseStats> getCustodianWiseStats();
 
     @Query(value="select c.client_id as clientId," +
-            "sum(if(ob.order_direction = 'SELL',ob.price * initial_quantity,0)) as totalSell," +
-            "sum(if(ob.order_direction = 'BUY',ob.price * initial_quantity,0)) as totalBuy " +
+            "sum(if(ob.order_direction = 'SELL' and ob.order_status='COMPLETED',ob.price * initial_quantity,0)) as totalSell," +
+            "sum(if(ob.order_direction = 'BUY' and ob.order_status='COMPLETED',ob.price * initial_quantity,0)) as totalBuy " +
             "from client c " +
             "left join order_book ob on c.client_id = ob.client_id " +
             "group by c.client_id",nativeQuery = true)
